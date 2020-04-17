eb9a5cf1797314bc479cbd7a451d776cb7f570b8
Saima Akhtar <saimaakhtar@gmail.com>
2020-04-17 18:56:16 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -40,8 +40,8 @@ def hit?(card_total)
   valid_inputs = ['h', 's']
   prompt_user
   get_user_input
-  if get_user_input == 'h'
-    card_total += deal_card
+ # if get_user_input == 'h'
+ #   card_total += deal_card
 
 end
 
